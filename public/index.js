474389788be96486e5f3e59cb53b1bac8268c89d
Vasyl Vasylyk <vasiliyworkplace@gmail.com>
2018-08-14 19:15:26 +0300

    add files

---
+++ public/index.js
@@ -0,0 +1,33 @@
+const currency = document.querySelector('.currency');
+const inputEl = currency.querySelector('.input');
+const getBtn = currency.querySelector('.get');
+const resEl = currency.querySelector('.result');
+const fromEl = currency.querySelector('.select-from');
+const toEl = currency.querySelector('.select-to');
+
+getBtn.addEventListener('click', getCurrency);
+
+function getCurrency() {
+	const fromVal = fromEl.value;
+	const toVal = toEl.value;
+	const query = `http://free.currencyconverterapi.com/api/v5/convert?q=${fromVal}_${toVal}&compact=y`;
+
+	fetch(query)
+		.then(resp=> {return resp.json()})
+		.then(data=>{
+			let rate = 1;
+
+			for (let item in data) {
+				rate = data[item].val;
+			}
+
+			calculate(rate);
+		})
+		.catch(() => resEl.innerText = 'Some problems with request');
+}
+
+function calculate(rate) {
+	const num = inputEl.value;
+
+	resEl.innerText = Math.round(rate * num * 100) / 100;
+}
