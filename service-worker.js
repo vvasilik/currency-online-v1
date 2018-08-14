a6e286a828148a13a7e393c5ee77c9c24bfd4b9e
Vasyl Vasylyk <vasiliyworkplace@gmail.com>
2018-08-14 22:25:32 +0300

    update version

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
+const version = 'v2';
+
 this.addEventListener('install', function(event) {
 	event.waitUntil(
-	  caches.open('v1').then(function(cache) {
+	  caches.open('version').then(function(cache) {
 		return cache.addAll([
 		  '/',
 		  '/index.html',
@@ -22,7 +24,7 @@ this.addEventListener('install', function(event) {
   });
 
   this.addEventListener('activate', function(event) {
-		var cacheWhitelist = ['v1'];
+		var cacheWhitelist = [version];
 		
 		event.waitUntil(
 			caches.keys().then(function(keyList) {
@@ -39,7 +41,7 @@ this.addEventListener('install', function(event) {
 		event.respondWith(
 			caches.match(event.request).then(function(resp) {
 				return resp || fetch(event.request).then(function(response) {
-					caches.open('v1').then(function(cache) {
+					caches.open(version).then(function(cache) {
 						cache.put(event.request, response)
 					});
 					return response.clone();
